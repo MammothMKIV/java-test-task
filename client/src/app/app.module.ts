757aa4cc6c74e07e15085475895f15068e6ca614
MammothMKIV <mammothmkiv@gmail.com>
2017-10-14 22:55:56 +0300

    Added bootstrap and setup base client structure

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,12 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from "@angular/router";
+import { BookListLayoutComponent } from "./layouts/book-list-layout/book-list-layout.component";
+import { HeaderComponent } from './components/header/header.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+
+const appRoutes = [
+  {
+    path: 'books',
+    component: BookListLayoutComponent
+  },
+  {
+    path: '',
+    redirectTo: '/books',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/books'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BookListLayoutComponent,
+    PaginationComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing : true }
+    ),
     BrowserModule
   ],
   providers: [],
---
+++ client/src/app/layouts/book-list-layout/book-list-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'book-list-layout',
+  templateUrl: './book-list-layout.component.html'
+})
+export class BookListLayoutComponent {
+  title = 'app';
+}
